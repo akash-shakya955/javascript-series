a5f82b9bd8183121d07d839b53b915eba3fd6b7a
Akash Shakya <mr.akashshakya@gmail.com>
2026-02-26 18:28:37 +0530

    Add Data Types Basics File

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+
+let name = "Akash";
+let age = 18;
+let isLoggedIn = false;
+
+
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+// object
+
+console.log(typeof null);
