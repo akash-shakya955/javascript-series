4e076dd056e2c9b45d2029423ddaf64ab1fa8b13
Akash Shakya <mr.akashshakya@gmail.com>
2026-02-27 23:23:53 +0530

    feat: Enhance Conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,5 +28,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ****************************************** OPERATIONS ******************************************************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+let str1 = "hello";
+let str2 = " Akash";
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100;
+
+// gameCounter++;
+++gameCounter;
+
+console.log(gameCounter);
+
+
+// learn prefix and postfix in mdn
+
+
+
+
