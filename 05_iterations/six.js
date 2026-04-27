2c633624aef58a2772dacb56978814e2da4d3725
Akash Shakya <mr.akashshakya@gmail.com>
2026-04-27 19:39:07 +0530

    add DOM basics and update iteration files

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -17,12 +17,15 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //     return num > 4
 // } )
 
-const newNums = []
+// const newNums = []
 
-myNums.forEach( (num) => {
-    if(num > 4){
-        newNums.push(num)
-    }
-} )
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// } )
 
-console.log(newNums);
+// console.log(newNums);
+
+
+
